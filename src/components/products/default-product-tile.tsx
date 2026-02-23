ebf9778673845212220d8734db1b4fdd22a440f0
Paal Gyula <gyula@pilab.hu>
2026-02-23 14:56:50 +0100

    fix: update ESLint Next.js plugin and guard unsafe product image access

--- src/components/products/default-product-tile.tsx
+++ src/components/products/default-product-tile.tsx
@@ -1,50 +1,12 @@
-import {Product} from "@/lib/client";
-
-type MediaResource = {
-  alt: string
-  url: {
-    alt: string
-    url: string
-    width: number
-    height: number
-    thumbnailURL: string
-  }
-}
-
-/**
- * Check if the resource is an image (type guard)
- * @param resource
- */
-export const isImage = (resource: any): resource is MediaResource => {
-  return (
-    typeof resource.alt === 'string' &&
-    typeof resource.url === 'object' &&
-    typeof resource.url.url === 'string' &&
-    typeof resource.url.width === 'number' &&
-    typeof resource.url.height === 'number' &&
-    typeof resource.url.thumbnailURL === 'string'
-  )
-}
-
-/**
- * Check if the product has an image
- * @param product
- */
-const hasImage = (product: Product): boolean => {
-  return product.images?.length > 0
-}
+import { Product } from '@/lib/client'
+import { hasProductImage } from '@/services/product-service'
 
 const DefaultProductTile: React.FC<{ product: Product }> = ({ product }) => {
-  if (!product || !hasImage(product)) {
+  if (!product || !hasProductImage(product)) {
     return null
   }
 
-  // console.log('DefaultProductTile product', isImage(product.images?.[0]))
-
-  // const productImage: MediaResource | undefined = isImage(product.images?.[0])
-  //   ? product.images?.[0]
-  //   : undefined
-  const productImage = product.images[0].url || '#'
+  const productImage = product.images?.[0]?.url || '#'
 
   return (
     <div className="transition-all flex flex-row shadow-sm hover:shadow-2xl hover:bg-gray-100">
